7ac6113ebf99bf477c64286d60653a04d4ae31b1
fullzer4 <gabrielpelizzaro@gmail.com>
2023-11-16 00:21:38 -0300

    robot

---
+++ Robot/Robot.ino
@@ -0,0 +1,17 @@
+#include <BluetoothSerial.h>
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("MeuESP32"); 
+}
+
+void loop() {
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+}
