434c8154d7e54c51663fa4ccf2c6dd075a5e530b
fullzer4 <gabrielpelizaro@gmail.com>
2023-11-27 23:45:53 -0300

    camera setup

--- App/pages/TestScreen.tsx
+++ App/pages/TestScreen.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React, {FC, useEffect, useState} from 'react';
+import {Text, View, StyleSheet, useWindowDimensions} from 'react-native';
 import {RouteProp} from '@react-navigation/native';
-import React, {FC} from 'react';
-import {Text, View} from 'react-native';
-import {RootStackParamList} from '../App';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {
+  useCameraDevices,
+  Camera,
+  useCameraPermission,
+} from 'react-native-vision-camera';
 import * as tf from '@tensorflow/tfjs';
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
 import '@tensorflow/tfjs-react-native';
 import Navbar from '../components/navbar';
+import {RootStackParamList} from '../App';
 
 type TestScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -21,14 +27,50 @@ type Props = {
 };
 
 const TestScreen: FC<Props> = ({route, navigation}) => {
+  const {hasPermission, requestPermission} = useCameraPermission();
+  const devices = useCameraDevices();
+  const [cameraDevice, setCameraDevice] = useState<any | null>(null);
+
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission, requestPermission]);
+
+  useEffect(() => {
+    const selectCameraDevice = async () => {
+      const availableDevices = await Camera.getAvailableCameraDevices();
+      if (availableDevices.length > 0) {
+        setCameraDevice(availableDevices[0].id);
+      }
+    };
+
+    selectCameraDevice();
+  }, []);
+
   return (
     <>
       <Navbar navigation={navigation} />
-      <View>
+      <View style={styles.container}>
         <Text>Test Screen</Text>
+        {hasPermission && cameraDevice && (
+          <Camera style={styles.camera} device={cameraDevice} isActive={true} />
+        )}
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  camera: {
+    width: '80%',
+    aspectRatio: 4 / 3,
+  },
+});
 
 export default TestScreen;
